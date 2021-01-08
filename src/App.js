871ce2a03006374852ac5220ce870e79d48c6165
simon louvet <simon.louvet.zen@gmail.com>
2021-01-08 01:37:09 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Graph from './graph';
+
+function App() {
+  return (
+    <div className="App">
+      <header >
+        <p>
+          D3JS test
+        </p>
+      </header>
+      <Graph></Graph>
+    </div>
+  );
+}
+
+export default App;
